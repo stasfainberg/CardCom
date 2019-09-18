4663495769e9f2f41c5e7953dba8a658b51baa3f
stasfainberg <stas.fainberg@gmail.com>
2019-09-18 13:18:16 +0300

    User-Model-Created

---
+++ CardCom/CardCom/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardCom.Models
+{
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+
+    public class User
+    {
+        public int ID { get; set; }
+
+        public string MyId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime BirthDay { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+
+
+
+
+
+}
